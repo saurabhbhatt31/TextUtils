0aa3471834a598e1b500166292252577f7d8025c
saurabh bhatt <saurabh.bhatt@hestabit.in>
2023-09-08 19:09:22 +0530

    select changes

--- src/Components/textform.js
+++ src/Components/textform.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function TextForm(props) {
+  const textvalue = useRef(null);
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
@@ -17,8 +18,10 @@ export default function TextForm(props) {
     props.alertmsg('   :Changed in Lower Case', 'success');
     setText(newText2);
   }
+  
   const copyToCliboard=()=>
   {
+    textvalue.current?.select();
     navigator.clipboard.writeText(text);
     props.alertmsg('   :Copied the text', 'success');
   } 
@@ -30,7 +33,7 @@ export default function TextForm(props) {
       <div className="container">
         <div className="my-3" style= {{color : props.mode==='light'?'black':'white'}}>
           <label labelfor="exampleFormControlTextarea1" className="form-label">{props.title1}</label>
-          <textarea className="form-control" onChange={handleOnchange} id="myTextBox" rows="3" value={text} style = {{color : props.mode==='light'?'black':'white',backgroundColor: props.mode==='dark'?'grey':'white'}}></textarea>
+          <textarea  ref={textvalue}  className="form-control" onChange={handleOnchange} id="myTextBox" rows="3" value={text} style = {{color : props.mode==='light'?'black':'white',backgroundColor: props.mode==='dark'?'grey':'white'}}></textarea>
         </div>
         
         <button type="button" className="btn btn-primary" onClick={handleUpClick}>Click to Chane In Upper Case</button>
